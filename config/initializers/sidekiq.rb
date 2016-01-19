6f02a03eada6b8cfae2b7deb35713fa0370d64da
Jason Pilz <jasonpilz@gmail.com>
2016-01-19 16:37:11 -0700

    Adjust procfile

--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://redis.example.com:17279/12' }
-end
-
-Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://redis.example.com:17279/12' }
-end
