a46a1a6622becbea790fac135809c5b77015ae26
Jason Pilz <jasonpilz@gmail.com>
2016-01-19 19:22:39 -0700

    Added redis-based worker back in

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,5 @@
 class DashboardsController < ApplicationController
   def index
-    # VehicleSaveWorker.perform_async(current_user.id)
-    vehicle_service = VehicleService.new(current_user)
-    vehicle_service.save_vehicles
+    VehicleSaveWorker.perform_async(current_user.id)
   end
 end
