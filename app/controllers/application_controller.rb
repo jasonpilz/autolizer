9c498aa9abf6fb265db467230d9bc1973932c119
Jason Pilz <jasonpilz@gmail.com>
2016-01-21 18:59:37 -0700

    Complete initial styling for trips index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
-
 end
--- test/integration/user_can_view_vehicle_stats_test.rb
+++ test/integration/user_can_view_vehicle_stats_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
-class UserCanViewVehicleStatsTest <
-  include Capybara::DSL
+# class UserCanViewVehicleStatsTest
+#   include Capybara::DSL
 
-  # setup do
-  #   Capybara.current_driver = Capybara.javascript_driver # :selenium by default
-  # end
+#   setup do
+#     Capybara.current_driver = Capybara.javascript_driver # :selenium by default
+#   end
 
-  # test "can view current stats for vehicle" do
+#   test "can view current stats for vehicle" do
 
-  #   session[:user_id] = users(:jason).id
-  #   visit "/dashboard"
+#     session[:user_id] = users(:jason).id
+#     visit "/dashboard"
 
-  #   assert page.has_content?("2012 Odyssey")
-  #   session.delete(:user_id)
-  # end
-end
+#     assert page.has_content?("2012 Odyssey")
+#     session.delete(:user_id)
+#   end
+# end
